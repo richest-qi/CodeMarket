561a96ec0555065998b531f6669ea14b0867dd82
richest_qi <752890913@qq.com>
2020-07-31 17:55:44 +0800

    开发环境不压缩图片，生产环境压缩图片

--- react/uncontrolled_components/webpack.common.js
+++ react/uncontrolled_components/webpack.common.js
@@ -42,28 +42,6 @@ module.exports = {
                             name:"[name].[ext]",
                             outputPath:"imgs",
                         }
-                    },
-                    {
-                        loader:"image-webpack-loader",
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65
-                            },
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            webp: {
-                                quality: 75
-                            }
-                            }                           
                     }
                 ]
             }
