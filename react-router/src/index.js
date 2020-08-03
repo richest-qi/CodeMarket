54bc490745949376d152d862f6f6c73dd793e91c
richest_qi <752890913@qq.com>
2020-08-03 15:53:06 +0800

    【react-router】添加app.css

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -6,3 +6,10 @@ ReactDOM.render(
     <ButtonGroup/>,
     document.querySelector('#buttonGroup')
 );
+
+// import BasicRoute from "./basicRouting/app.js";
+
+// ReactDOM.render(
+//     <BasicRoute/>,
+//     document.querySelector('#root')
+// );
