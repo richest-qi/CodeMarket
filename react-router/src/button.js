a752ae210cd6999b2e9a9876f18ddf4f9e0eda8c
richest_qi <752890913@qq.com>
2020-08-04 10:42:37 +0800

    【react-router】NavLink的activeClassName不生效之原因分析

--- react-router/src/button.js
+++ react-router/src/button.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React,{ Suspense } from "react";
 import ReactDOM from "react-dom";
+import loadable from "@loadable/component";
 
 const buttonStyle = {
     marginRight:"5px",
@@ -9,19 +10,34 @@ class Button extends React.Component{
     constructor(props){
         super(props);
         this.handleClick = () => {
-            const self = this;
-            async function getComponent(){
-                const component = await import(
-                     /*webpackPreload:true */
-                    `./${self.props.dirname}/app.js`);
-                return component.default || component;
-            }
-            getComponent().then(App => {
-                ReactDOM.render(
-                    <App/>,
-                    document.querySelector('#root')
-                )
-            })
+            // const self = this;
+            // async function getComponent(){
+            //     const component = await import(
+            //          /*webpackPreload:true */
+            //         `./${self.props.dirname}/app.js`);
+            //     return component.default || component;
+            // }
+            // getComponent().then(App => {
+            //     ReactDOM.render(
+            //         <App/>,
+            //         document.querySelector('#root')
+            //     )
+            // })
+
+            // const self = this;
+            // const App = React.lazy(() => import(`./${self.props.dirname}/app.js`));
+            // ReactDOM.render(
+            //     <Suspense fallback={<div>loading</div>}>
+            //         <App/>
+            //     </Suspense>,
+            //     document.querySelector('#root')
+            // )
+
+            const App = loadable(() => import(`./${this.props.dirname}/app.js`));
+            ReactDOM.render(
+                <App/>,
+                document.querySelector('#root')
+            )
         }
     }
     render(){
@@ -30,3 +46,4 @@ class Button extends React.Component{
 }
 
 export default Button;
+
