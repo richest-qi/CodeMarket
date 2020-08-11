94db7972715bbe01c27157bf9b8f30f0622b9d78
richest_qi <752890913@qq.com>
2020-08-11 22:14:43 +0800

    【vue】实现计数器

--- vue/counter_vue/src/counterPanel.vue
+++ vue/counter_vue/src/counterPanel.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="panel">
-        {
+        <!-- {
             values.map((value,idx) => <Counter 
                 v-bind:key="idx"
                 v-bind:value="value"
                 v-on:increment="handleIncrement(idx)"
                 v-on:decrement="handleDecrement(idx)"
             />)
-        }
-        <!-- <Counter v-for="(value,idx) in values" 
+        } -->
+        <Counter v-for="(value,idx) in values" 
                 v-bind:key="idx"
                 v-bind:value="value"
                 v-on:increment="handleIncrement(idx)"
                 v-on:decrement="handleDecrement(idx)"
-        /> -->
+        />
         <div class="sum">{{sum}}</div>
     </div>
 </template>
@@ -36,13 +36,13 @@ export default {
             return this.values.reduce((sum,value) => sum+=value,0)
         },
         handleIncrement:function(idx){
-            this.values[idx]++;
-            // this.values.splice(idx,1,this.values[idx]+1);
+            // this.values[idx]++;
+            this.values.splice(idx,1,this.values[idx]+1);
             // this.$set(this.values,idx,this.values[idx]+1);
         },
         handleDecrement:function(idx){
             this.values[idx]--;
-            // this.values.splice(idx,1,this.values[idx]-1);
+            this.values.splice(idx,1,this.values[idx]-1);
             // this.$set(this.values,idx,this.values[idx]-1);
         }
     },
