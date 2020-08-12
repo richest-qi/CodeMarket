036c3e060dba3f7f52ae7ea08ea64833753e7ae6
richest_qi <752890913@qq.com>
2020-08-12 22:03:58 +0800

    【vuex】使用计算属性从store中读取状态

--- vuex/counter/src/counter.vue
+++ vuex/counter/src/counter.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <span>{{$store.state.count}}</span>
+        <!-- <span>{{$store.state.count}}</span> -->
+        <span>{{count}}</span>
         <button v-on:click="handleIncrement">+</button>
         <button v-on:click="handleDecrement">-</button>
     </div>
@@ -8,6 +9,16 @@
 
 <script>
 export default {
+    // data:function(){
+    //     return {
+    //         count:this.$store.state.count
+    //     }
+    // },
+    computed:{
+        count:function(){
+            return this.$store.state.count
+        }
+    },
     methods:{
         handleIncrement:function(){
             this.$store.commit("increment");
