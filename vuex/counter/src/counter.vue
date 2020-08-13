f9285a8941c8f7ceb8d716f24b8c3b797eba607b
richest_qi <752890913@qq.com>
2020-08-13 22:25:42 +0800

    [vuex]state\getters\mutations\actions

--- vuex/counter/src/counter.vue
+++ vuex/counter/src/counter.vue
@@ -2,13 +2,19 @@
     <div>
         <!-- <span>{{$store.state.count}}</span> -->
         <span>{{count}} is {{evenOrOdd}}</span>
-        <button v-on:click="handleIncrement">+</button>
-        <button v-on:click="handleDecrement">-</button>
+        <!-- <button v-on:click="handleIncrement">+</button>
+        <button v-on:click="handleDecrement">-</button> -->
+        <!-- <button v-on:click="incrementAsync">+</button>
+        <button v-on:click="decrementAsync">-</button> -->
+        <button v-on:click="handleIncrementAsync">++</button>
+        <button v-on:click="handleDecrementAsync">--</button>
+        <!-- <button v-on:click="increment">+</button>
+        <button v-on:click="decrement">-</button> -->
     </div>
 </template>
 
 <script>
-import {mapState,mapGetters} from "vuex";
+import {mapState,mapGetters,mapMutations,mapActions} from "vuex";
 export default {
     // data:function(){
     //     return {
@@ -41,12 +47,93 @@ export default {
         ...mapState(['count']),
         ...mapGetters(['evenOrOdd'])
     },
+
+
+    // methods:{
+    //     handleIncrement:function(){
+    //         this.$store.commit("increment","hello","world");
+    //     },
+    //     handleDecrement:function(){
+    //         this.$store.commit("decrement","have","a","nice","day");
+    //     },
+    //     // handleIncrementAsync:function(){
+    //     //     this.$store.dispatch("incrementAsync");
+    //     // },
+    //     // handleDecrementAsync:function(){
+    //     //     this.$store.dispatch("decrementAsync");
+    //     // }
+    //     // handleIncrementAsync:function(){
+    //     //     this.$store.dispatch("incrementAsync",{
+    //     //         m:10
+    //     //     });
+    //     // },
+    //     // handleDecrementAsync:function(){
+    //     //     this.$store.dispatch("decrementAsync",{
+    //     //         n:5
+    //     //     })
+    //     // }
+    //     // handleIncrementAsync:function(){
+    //     //     this.$store.dispatch({
+    //     //         type:"incrementAsync",
+    //     //         m:10
+    //     //     })
+    //     // },
+    //     // handleDecrementAsync:function(){
+    //     //     this.$store.dispatch({
+    //     //         type:"decrementAsync",
+    //     //         n:5
+    //     //     })
+    //     // }
+    // }
+
+    // methods:mapMutations([
+    //     "increment",
+    //     "decrement"
+    // ]),
+
+    // methods:{
+    //     handleIncrement:function(){
+    //         this.$store.commit("increment",{
+    //             m:10
+    //         });
+    //     },
+    //     handleDecrement:function(){
+    //         this.$store.commit("decrement",{
+    //             n:5
+    //         });
+    //     }
+    // }
+
+    // methods:{
+    //     handleIncrement:function(){
+    //         this.$store.commit({
+    //             type:"increment",
+    //             m:10
+    //         })
+    //     },
+    //     handleDecrement:function(){
+    //         this.$store.commit({
+    //             type:"decrement",
+    //             n:5
+    //         })
+    //     }
+    // }
+    // methods:mapActions([
+    //     "incrementAsync",
+    //     "decrmentAsync"
+    // ])
+    // methods:mapActions({
+    //     handleIncrementAsync:"incrementAsync",
+    //     handleDecrementAsync:"decrementAsync"
+    // })
     methods:{
-        handleIncrement:function(){
-            this.$store.commit("increment");
+        handleIncrementAsync:function(){
+            this.$store.dispatch("incrementAsync").then(() => {
+                console.log("increment:done!");
+            })
         },
-        handleDecrement:function(){
-            this.$store.commit("decrement");
+        handleDecrementAsync:function(){
+            this.$store.dispatch("decrementAsync");
         }
     }
 }
--- vuex/counter/src/store.js
+++ vuex/counter/src/store.js
@@ -12,7 +12,6 @@ const getters = {
     }
 }
 
-
 const mutations = {
     increment:function(state){
         state.count++;
@@ -22,8 +21,86 @@ const mutations = {
     }
 }
 
+
+// const mutations = {
+//     increment:function(state,a,b){
+//         state.count++;
+//         console.log(a,b);
+//     },
+//     decrement:function(state,a,b,c,d){
+//         state.count--;
+//         console.log(a,b,c,d);
+//     }
+// }
+
+// const mutations = {
+//     increment:function(state,m){
+//         state.count+=m;
+//     },
+//     decrement:function(state,n){
+//         state.count-=n;
+//     }
+// }
+
+// const mutations = {
+//     increment:function(state,payload){
+//         state.count+=payload.m;
+//     },
+//     decrement:function(state,payload){
+//         state.count-=payload.n;
+//     }
+// }
+
+
+// const actions = {
+//     incrementAsync:function({commit},payload){
+//         setTimeout(() => {
+//             commit("increment",payload.m);
+//         },3000);
+//     },
+//     decrementAsync:function({commit},payload){
+//         setTimeout(() => {
+//             commit("decrement",payload.n);
+//         },1000);
+//     }
+// }
+
+
+// const actions = {
+//     incrementAsync:function({commit}){
+//         setTimeout(() => {
+//             commit("increment");
+//         },1000);
+//     },
+//     decrementAsync:function({commit}){
+//         setTimeout(() => {
+//             commit("decrement");
+//         },1000);
+//     }
+// }
+
+
+const actions = {
+    incrementAsync:function({commit,state}){
+        return new Promise(resolve => {
+            setTimeout(() => {
+                commit("increment");
+                console.log(state.count);
+                resolve();
+            },1000);
+        })
+    },
+    decrementAsync:async function({commit,dispatch,state}){
+        dispatch("incrementAsync");
+        await setTimeout(() => {
+            commit("decrement");
+            console.log("decrement:done!")
+        },1000);
+    }
+}
 export default new Vuex.Store({
     state,
     mutations,
-    getters
+    getters,
+    actions
 });
